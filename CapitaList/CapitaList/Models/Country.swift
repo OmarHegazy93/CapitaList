f056eb684086261f7e8e96465f097a951b7e6bc3
OmarHegazy93 <omarhegazy@mail.com>
2025-03-19 03:27:34 +0200

    Add country model and related changes

---
+++ CapitaList/CapitaList/Models/Country.swift
@@ -0,0 +1,45 @@
+//
+//  Models.swift
+//  CapitaList
+//
+//  Created by Omar Hegazy on 19/03/2025.
+//
+
+struct Country: Codable, Equatable, Identifiable {
+    let name: String
+    let code: String
+    let capital: String
+    private let latlngArray: [Double]
+    private let currenciesArray: [Currency]
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case code = "alpha3Code"
+        case capital
+        case latlngArray = "latlng"
+        case currenciesArray = "currencies"
+    }
+    
+    var id: String { code }
+    
+    var location: Location? {
+        guard latlngArray.count == 2 else { return nil }
+        return Location(latitude: latlngArray[0], longitude: latlngArray[1])
+    }
+    
+    var currencyString: String {
+        guard let currency = currenciesArray.first else { return "" }
+        return "\(currency.code) \(currency.name) \(currency.symbol)"
+    }
+}
+
+struct Currency: Codable, Equatable {
+    let code: String
+    let name: String
+    let symbol: String
+}
+
+struct Location {
+    let latitude: Double
+    let longitude: Double
+} 
